f3c4c2c93e0382fc9056e6b0d6acdafee8f2491b
souzaemanuella <emanuella.25104708360004@faeterj-rio.edu.br>
2025-09-29 22:07:34 -0300

    Add files via upload
    Js da página de pagamento.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('pagamento');
+const alerta = document.getElementById('alerta');
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    alerta.style.display = 'block';
+    setTimeout(() => {
+        alerta.style.display = 'none';
+        window.location.href = "index.html";
+    }, 3000);
+})
